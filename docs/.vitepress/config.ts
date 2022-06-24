4ebadb28c983adc6c06671d9b88c1d1ded56ff3f
alexzhang1030 <1642114555@qq.com>
2022-06-24 14:26:02 +0800

    chore: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,44 +2,37 @@ import { defineConfig } from 'vitepress'
 import Unocss from 'unocss/vite'
 import { presetAttributify, presetUno } from 'unocss'
 import { getNav } from './theme/data'
+import { getSidebar } from './theme/data/helper'
 
-export default defineConfig({
-  title: 'Alex\'s Website & Blog',
-  description: 'A personal website of Alex Zhang',
-  lastUpdated: true,
-  vite: {
-    plugins: [Unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
+export default async () => {
+  const sidebar = await getSidebar()
+  return defineConfig({
+    title: 'Alex\'s Website & Blog',
+    description: 'A personal website of Alex Zhang',
+    lastUpdated: true,
+    vite: {
+      plugins: [Unocss({
+        presets: [
+          presetAttributify(),
+          presetUno(),
+        ],
+      })],
+    },
+    themeConfig: {
+      footer: {
+        message: 'Love & Peace & Vue Ecosystem',
+        copyright: 'Copyright © 2022-present Alex Zhang',
+      },
+      nav: getNav(),
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/alexzhang1030/Blog' },
       ],
-    })],
-  },
-  themeConfig: {
-    footer: {
-      message: 'Love & Peace & Vue Ecosystem',
-      copyright: 'Copyright © 2022-present Alex Zhang',
+      algolia: {
+        appId: 'OOCSYYJ17Q',
+        indexName: 'alexzhang',
+        apiKey: 'a386a7bae0605420f6b4f0cc2eaf4f2a',
+      },
+      sidebar,
     },
-    nav: getNav(),
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/alexzhang1030/Blog' },
-    ],
-    algolia: {
-      appId: 'OOCSYYJ17Q',
-      indexName: 'alexzhang',
-      apiKey: 'a386a7bae0605420f6b4f0cc2eaf4f2a',
-    },
-    sidebar: {
-      '/Topic/Leetcode/Problems': [
-        {
-          text: 'Leetcode CN',
-          items: [
-            { text: '1. 两束值和', link: '/guide/' }, // /guide/index.md
-            { text: 'One', link: '/guide/one' }, // /guide/one.md
-            { text: 'Two', link: '/guide/two' }, // /guide/two.md
-          ],
-        },
-      ],
-    },
-  },
-})
+  })
+}
--- docs/.vitepress/theme/data/helper.ts
+++ docs/.vitepress/theme/data/helper.ts
@@ -1,15 +1,15 @@
 import type { DefaultTheme } from 'vitepress'
 import type { Leetcode, Topic } from '../../../../types'
+import TopicData from '../../../../data/topic.json'
 import LeetcodeData from '../../../../data/topic/leetcode.json'
-import TopicData from './topic'
 import NavData from './nav'
 
 export const TOPIC_PATH_PREFIX = '/Topic'
 
 export function getTopicData(): Topic[] {
   return TopicData.map(t => ({
-    name: t.name,
-    path: `${TOPIC_PATH_PREFIX}${t.path}`,
+    name: t.display_name,
+    path: `${TOPIC_PATH_PREFIX}${t.home_page}`,
     color: {
       bg: t.color.bg,
       text: t.color.text,
@@ -19,9 +19,25 @@ export function getTopicData(): Topic[] {
 
 export function getNav(): DefaultTheme.NavItem[] {
   return [...TopicData.map(t => ({
-    text: t.name,
-    link: `${TOPIC_PATH_PREFIX}${t.path}`,
+    text: t.display_name,
+    link: `${TOPIC_PATH_PREFIX}${t.home_page}`,
   })), ...NavData]
+}
+
+export async function getSidebar(): Promise<DefaultTheme.Sidebar> {
+  const result: DefaultTheme.Sidebar = {}
+  for (let i = 0; i < TopicData.length; i++) {
+    const t = TopicData[i]
+    const data = await import('../../../../data/topic/leetcode.json')
+    result[`${TOPIC_PATH_PREFIX}/${t.path_name}`] = [{
+      text: t.display_name,
+      items: data.map(item => ({
+        text: item.name,
+        link: item.path,
+      })),
+    }]
+  }
+  return result
 }
 
 export function getLeetcode(): Leetcode[] {
